c58a39199662e1f32ef091b042e3939362f5121f
telday <ejw393@gmail.com>
2021-10-31 14:19:03 -0400

    Added charts and updated data dict

--- create_data_dict.py
+++ create_data_dict.py
@@ -5,6 +5,7 @@ import sys
 import pandas
 import numpy as np
 import csv
+pandas.options.plotting.backend = "plotly"
 
 input_data = pathlib.Path('data/us.movies.actors.tsv') if len(sys.argv) < 2 else sys.argv[1]
 
@@ -46,10 +47,11 @@ cols = {
 
 
 # Start the output table off with the header names
-output_table = [['Variable', 'Unique Entries', 'Total Entries', 'Data Type', 'Data Classification', 'Number of Empty Values', 'Range']]
+output_table = [['Variable', 'Unique Entries', 'Total Entries', 'Data Type', 'Data Classification', 'Number of Empty Values', 'Range', 'Median', 'Mean', 'Mode', 'Min', 'Max', 'Outliers']]
 
 csv_dat = pandas.read_csv(input_data, sep='\t', na_values=['\\N'])
 for heading in csv_dat:
+    numeric = csv_dat[heading].dtype in (INT_TYPE, FLOAT_TYPE)
     new_row = [
             heading, # The Variable column
             csv_dat[heading].nunique(), # The Unique Entries column
@@ -57,9 +59,51 @@ for heading in csv_dat:
             data_type if (data_type := csv_dat[heading].dtype) != OBJECT_TYPE else 'String', # The Data Type column
             cols[heading], # Grab the DataClassification from the dict above
             csv_dat[heading].isna().sum(),
-            f'{csv_dat[heading].min()} - {csv_dat[heading].max()}' if csv_dat[heading].dtype in (INT_TYPE, FLOAT_TYPE) else None
+            f'{csv_dat[heading].min()} - {csv_dat[heading].max()}' if numeric else None
         ]
+    if numeric:
+        row_properties = [
+                csv_dat[heading].median(),
+                csv_dat[heading].mean(),
+                mode[0] if len(mode := csv_dat[heading].mode()) else None,
+                csv_dat[heading].min(),
+                csv_dat[heading].max(),
+                set(csv_dat[heading].loc[abs(csv_dat[heading] - csv_dat[heading].mean()) > csv_dat[heading].std() * 3])
+            ]
+        new_row += row_properties
+
     output_table.append(new_row)
+
+
+import plotly.graph_objects as go
+is_adult = go.Figure(data=[go.Pie(values=[(ones := csv_dat['isAdult'].sum()), csv_dat['isAdult'].count() - ones], labels=["1", "0"])])
+is_adult.update_layout(title_text="isAdult")
+is_adult.show()
+
+is_original_title = go.Figure(data=[go.Pie(values=[(ones := csv_dat['isOriginalTitle'].sum()), csv_dat['isOriginalTitle'].count() - ones], labels=["1", "0"])])
+is_original_title.update_layout(title_text="isOriginalTitle")
+is_original_title.show()
+
+runtime_minutes = csv_dat['runtimeMinutes'].plot.hist()
+runtime_minutes.show()
+
+ordering = csv_dat['ordering'].plot.hist()
+ordering.show()
+
+birth_year = csv_dat['birthYear'].plot.hist()
+birth_year.show()
+
+death_year = csv_dat['deathYear'].plot.hist()
+death_year.show()
+
+start_year = csv_dat['startYear'].plot.hist()
+start_year.show()
+
+rating = csv_dat['averageRating'].plot.hist()
+rating.show()
+
+votes = csv_dat['numVotes'].plot.box()
+votes.show()
 
 with open('data_dict.tsv', 'w') as csvF:
     writer = csv.writer(csvF, delimiter='\t')
